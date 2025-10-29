8dc4bfb6791e6ac1bd66bc5fbb017d508e576379
KyziaPi <148628839+KyziaPi@users.noreply.github.com>
2025-10-29 11:20:01 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,20 @@
-{
-  "extends": "eslint:recommended",
-  "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"]
-  }
-}
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {
+      semi: ["error", "always"],
+      "no-unused-vars": ["warn"],
+    },
+  },
+];
